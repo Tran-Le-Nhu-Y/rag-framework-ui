cf71a28707bb15672733fbe8c1a75b9d74caa84d
NhatBangLe <bangle37568@gmail.com>
2025-07-22 22:06:57 +0700

    add MUI locale

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,20 +1,26 @@
 import { createTheme } from '@mui/material';
+import { viVN as pickersViVN } from '@mui/x-date-pickers/locales';
+import { viVN as gridViVN } from '@mui/x-data-grid/locales';
 
-export const theme = createTheme({
-  typography: {
-    fontFamily: 'Roboto, Arial, sans-serif',
-    fontSize: 14,
-  },
-  components: {
-    // Name of the component
-    MuiButton: {
-      styleOverrides: {
-        // Name of the slot
-        root: {
-          // Some CSS
-          fontSize: '1rem',
+export const theme = createTheme(
+  {
+    typography: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+      fontSize: 14,
+    },
+    components: {
+      // Name of the component
+      MuiButton: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            fontSize: '1rem',
+          },
         },
       },
     },
   },
-});
+  pickersViVN,
+  gridViVN
+);
